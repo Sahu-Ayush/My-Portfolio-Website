c273d84338c056cb98d2f88e2fb482004582a39e
Sahu-Ayush <ayushsahu1@yahoo.com>
2020-06-22 14:12:59 +0530

    initial commit

---
+++ js/animate.js
@@ -0,0 +1,279 @@
+/*
+ * jQuery appear plugin
+ *
+ * Version: 0.3.3
+ */
+if ($(window).width() > 1024){
+
+(function($) {
+    var selectors = [];
+
+    var check_binded = false;
+    var check_lock = false;
+    var defaults = {
+        interval: 250,
+        force_process: false
+    }
+    var $window = $(window);
+
+    var $prior_appeared;
+
+    function process() {
+        check_lock = false;
+        for (var index = 0; index < selectors.length; index++) {
+            var $appeared = $(selectors[index]).filter(function() {
+                return $(this).is(':appeared');
+            });
+
+            $appeared.trigger('appear', [$appeared]);
+
+            if ($prior_appeared) {
+                var $disappeared = $prior_appeared.not($appeared);
+                $disappeared.trigger('disappear', [$disappeared]);
+            }
+            $prior_appeared = $appeared;
+        }
+    }
+
+    // "appeared" custom filter
+    $.expr[':']['appeared'] = function(element) {
+        var $element = $(element);
+        if (!$element.is(':visible')) {
+            return false;
+        }
+
+        var window_left = $window.scrollLeft();
+        var window_top = $window.scrollTop();
+        var offset = $element.offset();
+        var left = offset.left;
+        var top = offset.top;
+
+        if (top + $element.height() >= window_top &&
+            top - ($element.data('appear-top-offset') || 0) <= window_top + $window.height() &&
+            left + $element.width() >= window_left &&
+            left - ($element.data('appear-left-offset') || 0) <= window_left + $window.width()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    $.fn.extend({
+        // watching for element's appearance in browser viewport
+        appear: function(options) {
+            var opts = $.extend({}, defaults, options || {});
+            var selector = this.selector || this;
+            if (!check_binded) {
+                var on_check = function() {
+                    if (check_lock) {
+                        return;
+                    }
+                    check_lock = true;
+
+                    setTimeout(process, opts.interval);
+                };
+
+                $(window).scroll(on_check).resize(on_check);
+                check_binded = true;
+            }
+
+            if (opts.force_process) {
+                setTimeout(process, opts.interval);
+            }
+            selectors.push(selector);
+            return $(selector);
+        }
+    });
+
+    $.extend({
+        // force elements's appearance check
+        force_appear: function() {
+            if (check_binded) {
+                process();
+                return true;
+            };
+            return false;
+        }
+    });
+})(jQuery);
+
+var isMobile = false;
+
+(function( $ ) {
+    "use strict";
+
+    $(function() {
+
+        /* --- MOBILE DETECT --- */
+        if (navigator.userAgent.match(/Android/i) ||
+            navigator.userAgent.match(/webOS/i) ||
+            navigator.userAgent.match(/iPhone/i) ||
+            navigator.userAgent.match(/iPad/i)||
+            navigator.userAgent.match(/iPod/i) ||
+            navigator.userAgent.match(/BlackBerry/i)) {
+            isMobile = true;
+        }
+
+        /* --- ADD NECESSARY CLASS --- */
+        if (isMobile == true) {
+            $('.animated').removeClass('animated');
+            $('.op0').removeClass('op0');
+        }
+
+        /* --- ANIMATE CONTENT --- */
+        if (isMobile == false) {
+            $('*[data-animated]').addClass('animated');
+        }
+
+        function animated_contents() {
+            $(".animated:appeared").each(function (i) {
+                var $this    = $(this),
+                    animated = $(this).data('animated');
+
+                setTimeout(function () {
+                    $this.addClass(animated);
+                }, 100 * i);
+
+            });
+        }
+
+        if (isMobile == false) {
+            animated_contents();
+            $(window).scroll(function () {
+                animated_contents();
+            });
+        }
+    });
+
+}(jQuery));
+
+}
+
+
+
+/* ################## Text Animation Code ################### */
+
+
+
+
+// const TypeWriter = function(txtElement, words, wait = 3000) {
+//   this.txtElement = txtElement;
+//   this.words = words;
+//   this.txt = '';
+//   this.wordIndex = 0;
+//   this.wait = parseInt(wait, 10);
+//   this.type();
+//   this.isDeleting = false;
+// }
+
+// // Type Method
+// TypeWriter.prototype.type = function() {
+//   // Current index of word
+//   const current = this.wordIndex % this.words.length;
+//   // Get full text of current word
+//   const fullTxt = this.words[current];
+
+//   // Check if deleting
+//   if(this.isDeleting) {
+//     // Remove char
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     // Add char
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
+
+//   // Insert txt into element
+//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+//   // Initial Type Speed
+//   let typeSpeed = 300;
+
+//   if(this.isDeleting) {
+//     typeSpeed /= 2;
+//   }
+
+//   // If word is complete
+//   if(!this.isDeleting && this.txt === fullTxt) {
+//     // Make pause at end
+//     typeSpeed = this.wait;
+//     // Set delete to true
+//     this.isDeleting = true;
+//   } else if(this.isDeleting && this.txt === '') {
+//     this.isDeleting = false;
+//     // Move to next word
+//     this.wordIndex++;
+//     // Pause before start typing
+//     typeSpeed = 500;
+//   }
+
+//   setTimeout(() => this.type(), typeSpeed);
+// }
+
+
+// ES6 Class
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
